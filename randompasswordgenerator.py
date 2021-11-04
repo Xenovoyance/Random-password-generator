b1a7afd0a346a1414ca6c79bcd217a4780c0fb48
Martin Karlsson <martin@pmkarlsson.com>
2021-11-04 09:42:16 +0100

    Update randompasswordgenerator.py
    Password generation

--- randompasswordgenerator.py
+++ randompasswordgenerator.py
@@ -1,6 +1,8 @@
 import string
 import random
 
-def password_generator()
+## Creeate a string of upper- and lower-case letters, numbers and punctation marks. Out of this string, generate strings of default 25 char
+def password_generator(size=25, chars=string.ascii_uppercase + string.ascii_lowercase + string.digits + string.punctuation):
+    return ''.join(random.choice(chars) for _ in range(size))
 
 print(password_generator())
