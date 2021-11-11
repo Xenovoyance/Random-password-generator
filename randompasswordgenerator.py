325ba2bb3831073b6226e312102a3716bb265bcf
Martin Karlsson <xenovoyance@gmail.com>
2021-11-11 14:00:31 +0100

    Removed some unsupported special characters

--- randompasswordgenerator.py
+++ randompasswordgenerator.py
@@ -3,7 +3,7 @@ import random
 import subprocess 
 
 ## Creeate a string of upper- and lower-case letters, numbers and punctation marks. Out of this string, generate strings of default 25 char
-def password_generator(size=25, chars=string.ascii_uppercase + string.ascii_lowercase + string.digits + string.punctuation):
+def password_generator(size=25, chars=string.ascii_uppercase + string.ascii_lowercase + string.digits + "!?/!?/.,:"):
     return ''.join(random.choice(chars) for _ in range(size))
 
 generated_password = password_generator()
@@ -23,9 +23,9 @@ try:
 except FileNotFoundError:
     print('INFO >> Not running Windows')
 
-# Try to copy the generated password to clipboard (MacOS)
-try:
-    subprocess.run("xclip", universal_newlines=True, input=password_generator())
-    print('INFO >> Password pasted to clipboard (Linux/xclip)')
-except FileNotFoundError:
-    print('INFO >> Not running Linux')
+# Try to copy the generated password to clipboard (Linux)
+#try:
+#    subprocess.run("xclip", universal_newlines=True, input=password_generator())
+#    print('INFO >> Password pasted to clipboard (Linux/xclip)')
+#except FileNotFoundError:
+#   print('INFO >> Not running Linux')
